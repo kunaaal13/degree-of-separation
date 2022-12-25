aac961b121e9a65461d9c73b0335910b99ce6797
Kunaaal13 <mrkunalyadav7@gmail.com>
2022-12-25 20:01:24 +0530

    minor fixes

--- components/Home/UserCard.tsx
+++ components/Home/UserCard.tsx
@@ -16,22 +16,24 @@ function UserCard({ user, setSelectedUser, setSelectedMenu }: Props) {
   }, [isActive])
 
   return (
-    <div className='bg-gray-100 dark:bg-gray-700 dark:shadow-gray-700 rounded-md border shadow-md m-3 h-32 px-3 flex items-center'>
+    <div
+      onClick={() => {
+        setSelectedUser(user)
+        setSelectedMenu('user')
+      }}
+      className='bg-gray-100 dark:bg-gray-700 dark:shadow-gray-700 rounded-md border shadow-md m-3 h-32 px-3 flex items-center'
+    >
       {/* image */}
-      <div className='h-20 w-20 rounded-full'>
+      <div className='h-20 w-20 rounded-full cursor-pointer'>
         <img
           src={user.img}
           alt={user.name}
-          className='h-full w-full rounded-full cursor-pointer'
-          onClick={() => {
-            setSelectedUser(user)
-            setSelectedMenu('user')
-          }}
+          className='h-full w-full rounded-full'
         />
       </div>
 
       {/* Details */}
-      <div className='ml-4 w-3/5'>
+      <div className='ml-4 w-3/5 cursor-pointer'>
         <h1 className='text-lg md:text-xl font-semibold line-clamp-1'>
           {user.name}
         </h1>
@@ -40,7 +42,7 @@ function UserCard({ user, setSelectedUser, setSelectedMenu }: Props) {
           @{user.username}
         </p>
 
-        <div className='flex items-center justify-between mt-2'>
+        <div className='flex items-center justify-between mt-2 cursor-pointer'>
           <p className=''>{user.friends.length} friends</p>
 
           <p className={`${isActive ? 'text-green-500' : 'text-red-500'}`}>
